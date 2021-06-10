33ce6034153387b4893498b63e4cb5087ad15253
Brian Lee <brian.ts.lee.0907@gmail.com>
2021-06-10 22:21:55 +0900

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Brian Lee on 2021/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Brian Lee on 2021/06/10.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
